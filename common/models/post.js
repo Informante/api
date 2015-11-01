a20144f77ee79a6aeb455f2647c29c2ed4c2471b
Paulo McNally <paulomcnally@gmail.com>
2015-10-31 22:33:46 -0600

    added user relation

--- common/models/post.js
+++ common/models/post.js
@@ -3,20 +3,45 @@ module.exports = function(Post) {
   Post.feed = function(cb) {
     Post.find({
       limit: 15,
-      include: 'postType'
+      include: [
+        {
+          relation: 'postType',
+          scope: {
+            fields: [
+              'name'
+            ]
+          }
+        },
+        {
+          relation: 'user', // include the owner object
+          scope: { // further filter the owner object
+            fields: [
+              'name'
+            ], // only show two fields
+            include: { // include orders for the owner
+              relation: 'identities',
+              scope: {
+                fields: [
+                  'profile'
+                ]
+              }
+            }
+          }
+        }
+      ]
     }, cb);
   }
 
   Post.remoteMethod('feed',
-    {
-      http: {
-        verb: 'get'
-      },
-      returns: {
-        root: true,
-        type: 'object'
-      }
+  {
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      root: true,
+      type: 'object'
     }
-  );
+  }
+);
 
 };
