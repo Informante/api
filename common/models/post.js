11ae5bfe810ea76cfb7cff89db5d9ed2d6ad77ab
Paulo McNally <paulomcnally@gmail.com>
2015-10-31 20:55:03 -0600

    added feed

--- common/models/post.js
+++ common/models/post.js
@@ -1,3 +1,19 @@
 module.exports = function(Post) {
 
+  Post.feed = function(cb) {
+    Post.find({
+      limit: 15,
+      include: 'postType'
+    }, cb);
+  }
+
+  Post.remoteMethod('feed',
+    {
+      returns: {
+        root: true,
+        type: 'object'
+      }
+    }
+  );
+
 };
