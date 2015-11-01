b55426d1de81dafb295fd24557a7aa032b9ca16c
Paulo McNally <paulomcnally@gmail.com>
2015-11-01 11:44:37 -0600

    added all method and added ACL to PostType model

--- common/models/post-type.js
+++ common/models/post-type.js
@@ -1,3 +1,17 @@
 module.exports = function(PostType) {
 
+  PostType.all = function(id, cb) {
+    PostType.find({}, cb);
+  }
+
+  PostType.remoteMethod('all', {
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      root: true,
+      type: 'object'
+    }
+  });
+
 };
