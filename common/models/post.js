a5316f5a849fdf652c856de926b15acae4a4666b
Paulo McNally <paulomcnally@gmail.com>
2015-11-07 00:33:39 -0600

    added read method

--- common/models/post.js
+++ common/models/post.js
@@ -27,69 +27,69 @@ module.exports = function(Post) {
     }, cb);
   }
 
-   Post.getApp(function(err, app) {
-     var Report = app.models.Report;
+  Post.getApp(function(err, app) {
+    var Report = app.models.Report;
 
-     // Report a post
-     Post.report = function(id, cb) {
-       // set current context
-       var ctx = loopback.getCurrentContext();
-       // set userId based accessToken
-       var userId = ctx.active.http.req.accessToken.userId;
-       // find post by id
-       Post.findById(id, function(err, post) {
-         if (err) {
-           cb(null, 'Error al intentar comprobar la existencia de la publicación.');
-         }
-         else if (post) {
-           Report.find({
-             where: {
-               post_id: id,
-               user_id: userId
-             }
-           }, function(err, report) {
-             if (err) {
-               cb(null, 'Error al intentar comprobar el reporte.');
-             }
-             else if (report.length > 0) {
-               // report exists
-               cb(null, 'Ya has reportado esta publicación.');
-             }
-             else {
-               // create report
-               Report.create({
-                 post_id: id,
-                 user_id: userId,
-                 created_at: new Date()
-               }, function(err, reportCreated) {
-                 if (err) {
-                   cb(null, 'Error al intentar crear el reporte.');
-                 }
-                 else if(reportCreated) {
-                   cb(null, 'Tu reporte se ha generado.');
-                 }
-               });
-             }
-           });
-         }
-         else {
-           cb(null, 'No existe la publicación.');
-         }
-       });
-     }
+    // Report a post
+    Post.report = function(id, cb) {
+      // set current context
+      var ctx = loopback.getCurrentContext();
+      // set userId based accessToken
+      var userId = ctx.active.http.req.accessToken.userId;
+      // find post by id
+      Post.findById(id, function(err, post) {
+        if (err) {
+          cb(null, 'Error al intentar comprobar la existencia de la publicación.');
+        }
+        else if (post) {
+          Report.find({
+            where: {
+              post_id: id,
+              user_id: userId
+            }
+          }, function(err, report) {
+            if (err) {
+              cb(null, 'Error al intentar comprobar el reporte.');
+            }
+            else if (report.length > 0) {
+              // report exists
+              cb(null, 'Ya has reportado esta publicación.');
+            }
+            else {
+              // create report
+              Report.create({
+                post_id: id,
+                user_id: userId,
+                created_at: new Date()
+              }, function(err, reportCreated) {
+                if (err) {
+                  cb(null, 'Error al intentar crear el reporte.');
+                }
+                else if(reportCreated) {
+                  cb(null, 'Tu reporte se ha generado.');
+                }
+              });
+            }
+          });
+        }
+        else {
+          cb(null, 'No existe la publicación.');
+        }
+      });
+    }
 
-     Post.remoteMethod('report', {
-       accepts: {
-         arg: 'id',
-         type: 'string',
-         required: true
-       },
-       returns: {
-         root: true,
-         type: 'string'
-       }
-     });
-   });
+    Post.remoteMethod('report', {
+      accepts: {
+        arg: 'id',
+        type: 'string',
+        required: true
+      },
+      returns: {
+        root: true,
+        type: 'string'
+      }
+    });
+  });
 
   // Added or remove likes to post
   Post.like = function(id, cb) {
@@ -120,6 +120,49 @@ module.exports = function(Post) {
       }
       else {
         cb(null, 0);
+      }
+    });
+  }
+
+  // data
+  Post.read = function(id, cb) {
+    Post.findById(id, {
+      include: [
+        {
+          relation: 'postType',
+          scope: {
+            fields: [
+              'name'
+            ]
+          }
+        },
+        {
+          relation: 'user', // include the owner object
+          scope: { // further filter the owner object
+            fields: [
+              'name'
+            ], // only show two fields
+            include: { // include orders for the owner
+              relation: 'identities',
+              scope: {
+                fields: [
+                  'profile'
+                ]
+              }
+            }
+          }
+        }
+      ]
+    }, function(err, post) {
+      if (err) {
+        cb(err, null);
+      }
+      else if (post) {
+        post.created_at_format = moment(post.created_at).fromNow();
+        cb(null, post);
+      }
+      else {
+        cb(null, []);
       }
     });
   }
@@ -216,6 +259,21 @@ module.exports = function(Post) {
     returns: {
       root: true,
       type: 'number'
+    }
+  });
+
+  Post.remoteMethod('read', {
+    accepts: {
+      arg: 'id',
+      type: 'string',
+      required: true
+    },
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      root: true,
+      type: 'object'
     }
   });
 
