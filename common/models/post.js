13d3e764767c5fd5da1d77d7c11cd20f236fb84e
Paulo McNally <paulomcnally@gmail.com>
2015-11-11 00:40:45 -0600

    disable remote methods and enable countLike and countComment

--- common/models/post.js
+++ common/models/post.js
@@ -6,8 +6,21 @@ moment.locale('es');
 
 module.exports = function(Post) {
 
+  // disable remote methods
   Post.disableRemoteMethod('__get__postType', false);
   Post.disableRemoteMethod('__get__user', false);
+  Post.disableRemoteMethod('__findById__comments', false);
+  Post.disableRemoteMethod('__destroyById__comments', false);
+  Post.disableRemoteMethod('__updateById__comments', false);
+  Post.disableRemoteMethod('__exists__comments', false);
+  Post.disableRemoteMethod('__link__comments', false);
+  Post.disableRemoteMethod('__get__comments', false);
+  Post.disableRemoteMethod('__create__comments', false);
+  Post.disableRemoteMethod('__update__comments', false);
+  Post.disableRemoteMethod('__destroy__comments', false);
+  Post.disableRemoteMethod('__unlink__comments', false);
+  Post.disableRemoteMethod('__count__comments', false);
+  Post.disableRemoteMethod('__delete__comments', false);
 
   Post.generate = function(image, description, lat, lng, post_type_id,  cb) {
     // set current context
@@ -84,34 +97,95 @@ module.exports = function(Post) {
                 })
               }
             });
-        }
-        else {
-          cb(null, 'No existe una denuncia con el identificador enviado.');
+          }
+          else {
+            cb(null, 'No existe una denuncia con el identificador enviado.');
+          }
+        });
+      }
+
+      Post.remoteMethod('comment', {
+        accepts: [
+          {
+            arg: 'id',
+            type: 'string',
+            required: true
+          },
+          {
+            arg: 'message',
+            type: 'string',
+            required: true
+          }
+        ],
+        returns: {
+          root: true,
+          type: 'object'
         }
       });
-    }
 
-    Post.remoteMethod('comment', {
-      accepts: [
-        {
+      // Report a post
+      Post.report = function(id, cb) {
+        // set current context
+        var ctx = loopback.getCurrentContext();
+        // set userId based accessToken
+        var userId = ctx.active.http.req.accessToken.userId;
+        // find post by id
+        Post.findById(id, function(err, post) {
+          if (err) {
+            cb(null, 'Error al intentar comprobar la existencia de la publicación.');
+          }
+          else if (post) {
+            Report.find({
+              where: {
+                post_id: id,
+                user_id: userId
+              }
+            }, function(err, report) {
+              if (err) {
+                cb(null, 'Error al intentar comprobar el reporte.');
+              }
+              else if (report.length > 0) {
+                // report exists
+                cb(null, 'Ya has reportado esta publicación.');
+              }
+              else {
+                // create report
+                Report.create({
+                  post_id: id,
+                  user_id: userId,
+                  created_at: new Date()
+                }, function(err, reportCreated) {
+                  if (err) {
+                    cb(null, 'Error al intentar crear el reporte.');
+                  }
+                  else if(reportCreated) {
+                    cb(null, 'Tu reporte se ha generado.');
+                  }
+                });
+              }
+            });
+          }
+          else {
+            cb(null, 'No existe la publicación.');
+          }
+        });
+      }
+
+      Post.remoteMethod('report', {
+        accepts: {
           arg: 'id',
           type: 'string',
           required: true
         },
-        {
-          arg: 'message',
-          type: 'string',
-          required: true
+        returns: {
+          root: true,
+          type: 'string'
         }
-      ],
-      returns: {
-        root: true,
-        type: 'object'
-      }
+      });
     });
 
-    // Report a post
-    Post.report = function(id, cb) {
+    // Added or remove likes to post
+    Post.like = function(id, cb) {
       // set current context
       var ctx = loopback.getCurrentContext();
       // set userId based accessToken
@@ -119,46 +193,195 @@ module.exports = function(Post) {
       // find post by id
       Post.findById(id, function(err, post) {
         if (err) {
-          cb(null, 'Error al intentar comprobar la existencia de la publicación.');
+          cb(null, 0);
         }
         else if (post) {
-          Report.find({
-            where: {
-              post_id: id,
-              user_id: userId
-            }
-          }, function(err, report) {
-            if (err) {
-              cb(null, 'Error al intentar comprobar el reporte.');
-            }
-            else if (report.length > 0) {
-              // report exists
-              cb(null, 'Ya has reportado esta publicación.');
-            }
-            else {
-              // create report
-              Report.create({
-                post_id: id,
-                user_id: userId,
-                created_at: new Date()
-              }, function(err, reportCreated) {
-                if (err) {
-                  cb(null, 'Error al intentar crear el reporte.');
-                }
-                else if(reportCreated) {
-                  cb(null, 'Tu reporte se ha generado.');
-                }
-              });
-            }
-          });
+          var item = userId.toString();
+          var position = post.likes.indexOf(item);
+          if (position===-1) {
+            post.likes.push(item);
+            post.save(function(err) {
+              cb(null, post.likes.length);
+            });
+          }
+          else {
+            post.likes.splice(position, 1);
+            post.save(function(err) {
+              cb(null, post.likes.length);
+            });
+          }
         }
         else {
-          cb(null, 'No existe la publicación.');
+          cb(null, 0);
         }
       });
     }
 
-    Post.remoteMethod('report', {
+    // data
+    Post.read = function(id, cb) {
+      Post.findById(id, {
+        include: [
+          {
+            relation: 'comments',
+            scope: {
+              include: { // include orders for the owner
+                relation: 'user',
+                scope: {
+                  fields: [
+                    'id'
+                  ],
+                  include: { // include orders for the owner
+                    relation: 'identities',
+                    scope: {
+                      fields: [
+                        'profile'
+                      ]
+                    }
+                  }
+                }
+              }
+            }
+          },
+          {
+            relation: 'postType',
+            scope: {
+              fields: [
+                'name'
+              ]
+            }
+          },
+          {
+            relation: 'user', // include the owner object
+            scope: { // further filter the owner object
+              fields: [
+                'name'
+              ], // only show two fields
+              include: { // include orders for the owner
+                relation: 'identities',
+                scope: {
+                  fields: [
+                    'profile'
+                  ]
+                }
+              }
+            }
+          }
+        ]
+      }, function(err, post) {
+        if (err) {
+          cb(err, null);
+        }
+        else if (post) {
+          var postObject = post.toJSON();
+          var comments = [];
+          postObject.countLike = numeral(postObject.likes.length).format('0a');
+          postObject.countComment = numeral(postObject.comments.length).format('0a');
+          postObject.created_at_format = moment(postObject.created_at).fromNow();
+          if (postObject.comments.length > 0) {
+            postObject.comments.forEach(function(comment) {
+              comment.created_at_format = moment(comment.created_at).fromNow();
+              comments.push(comment);
+            });
+            postObject.comments = comments;
+          }
+          cb(null, postObject);
+        }
+        else {
+          cb(null, []);
+        }
+      });
+    }
+
+    // latest posts
+    Post.feed = function(cb) {
+      Post.find({
+        order: 'created_at DESC',
+        limit: 15,
+        include: [
+          {
+            relation: 'comments'
+          },
+          {
+            relation: 'postType',
+            scope: {
+              fields: [
+                'name'
+              ]
+            }
+          },
+          {
+            relation: 'user', // include the owner object
+            scope: { // further filter the owner object
+              fields: [
+                'name'
+              ], // only show two fields
+              include: { // include orders for the owner
+                relation: 'identities',
+                scope: {
+                  fields: [
+                    'profile'
+                  ]
+                }
+              }
+            }
+          }
+        ]
+      }, function(err, posts) {
+        if (err) {
+          cb(err, null);
+        }
+        else if (posts) {
+          var allPost = [];
+          posts.forEach(function(post) {
+            var postObject = post.toJSON();
+            postObject.countLike = numeral(postObject.likes.length).format('0a');
+            postObject.countComment = numeral(postObject.comments.length).format('0a');
+            postObject.created_at_format = moment(post.created_at).fromNow();
+            allPost.push(postObject);
+          })
+          cb(null, allPost);
+        }
+        else {
+          cb(null, []);
+        }
+      });
+    }
+
+    Post.remoteMethod('generate', {
+      accepts: [
+        {
+          arg: 'image',
+          type: 'string',
+          required: true
+        },
+        {
+          arg: 'description',
+          type: 'string',
+          required: true
+        },
+        {
+          arg: 'lat',
+          type: 'string',
+          required: true
+        },
+        {
+          arg: 'lng',
+          type: 'string',
+          required: true
+        },
+        {
+          arg: 'post_type_id',
+          type: 'string',
+          required: true
+        }
+      ],
+      returns: {
+        root: true,
+        type: 'number'
+      }
+    });
+
+    Post.remoteMethod('like', {
       accepts: {
         arg: 'id',
         type: 'string',
@@ -166,240 +389,33 @@ module.exports = function(Post) {
       },
       returns: {
         root: true,
-        type: 'string'
+        type: 'number'
       }
     });
-  });
 
-  // Added or remove likes to post
-  Post.like = function(id, cb) {
-    // set current context
-    var ctx = loopback.getCurrentContext();
-    // set userId based accessToken
-    var userId = ctx.active.http.req.accessToken.userId;
-    // find post by id
-    Post.findById(id, function(err, post) {
-      if (err) {
-        cb(null, 0);
-      }
-      else if (post) {
-        var item = userId.toString();
-        var position = post.likes.indexOf(item);
-        if (position===-1) {
-          post.likes.push(item);
-          post.save(function(err) {
-            cb(null, post.likes.length);
-          });
-        }
-        else {
-          post.likes.splice(position, 1);
-          post.save(function(err) {
-            cb(null, post.likes.length);
-          });
-        }
-      }
-      else {
-        cb(null, 0);
-      }
-    });
-  }
-
-  // data
-  Post.read = function(id, cb) {
-    Post.findById(id, {
-      include: [
-        {
-          relation: 'comments',
-          scope: {
-            include: { // include orders for the owner
-              relation: 'user',
-              scope: {
-                fields: [
-                  'id'
-                ],
-                include: { // include orders for the owner
-                  relation: 'identities',
-                  scope: {
-                    fields: [
-                      'profile'
-                    ]
-                  }
-                }
-              }
-            }
-          }
-        },
-        {
-          relation: 'postType',
-          scope: {
-            fields: [
-              'name'
-            ]
-          }
-        },
-        {
-          relation: 'user', // include the owner object
-          scope: { // further filter the owner object
-            fields: [
-              'name'
-            ], // only show two fields
-            include: { // include orders for the owner
-              relation: 'identities',
-              scope: {
-                fields: [
-                  'profile'
-                ]
-              }
-            }
-          }
-        }
-      ]
-    }, function(err, post) {
-      if (err) {
-        cb(err, null);
-      }
-      else if (post) {
-        var postObject = post.toJSON();
-        var comments = [];
-        postObject.countLike = numeral(post.likes.length).format('0a');
-        postObject.countComment = numeral(postObject.comments.length).format('0a');
-        postObject.created_at_format = moment(post.created_at).fromNow();
-        if (postObject.comments.length > 0) {
-          postObject.comments.forEach(function(comment) {
-            comment.created_at_format = moment(comment.created_at).fromNow();
-            comments.push(comment);
-          });
-          postObject.comments = comments;
-        }
-        cb(null, postObject);
-      }
-      else {
-        cb(null, []);
-      }
-    });
-  }
-
-  // latest posts
-  Post.feed = function(cb) {
-    Post.find({
-      order: 'created_at DESC',
-      limit: 15,
-      include: [
-        {
-          relation: 'comments'
-        },
-        {
-          relation: 'postType',
-          scope: {
-            fields: [
-              'name'
-            ]
-          }
-        },
-        {
-          relation: 'user', // include the owner object
-          scope: { // further filter the owner object
-            fields: [
-              'name'
-            ], // only show two fields
-            include: { // include orders for the owner
-              relation: 'identities',
-              scope: {
-                fields: [
-                  'profile'
-                ]
-              }
-            }
-          }
-        }
-      ]
-    }, function(err, posts) {
-      if (err) {
-        cb(err, null);
-      }
-      else if (posts) {
-        var allPost = [];
-        posts.forEach(function(post) {
-          post.created_at_format = moment(post.created_at).fromNow();
-          allPost.push(post);
-        })
-        cb(null, allPost);
-      }
-      else {
-        cb(null, []);
-      }
-    });
-  }
-
-  Post.remoteMethod('generate', {
-    accepts: [
-      {
-        arg: 'image',
+    Post.remoteMethod('read', {
+      accepts: {
+        arg: 'id',
         type: 'string',
         required: true
       },
-      {
-        arg: 'description',
-        type: 'string',
-        required: true
+      http: {
+        verb: 'get'
       },
-      {
-        arg: 'lat',
-        type: 'string',
-        required: true
-      },
-      {
-        arg: 'lng',
-        type: 'string',
-        required: true
-      },
-      {
-        arg: 'post_type_id',
-        type: 'string',
-        required: true
+      returns: {
+        root: true,
+        type: 'object'
       }
-    ],
-    returns: {
-      root: true,
-      type: 'number'
-    }
-  });
+    });
 
-  Post.remoteMethod('like', {
-    accepts: {
-      arg: 'id',
-      type: 'string',
-      required: true
-    },
-    returns: {
-      root: true,
-      type: 'number'
-    }
-  });
+    Post.remoteMethod('feed', {
+      http: {
+        verb: 'get'
+      },
+      returns: {
+        root: true,
+        type: 'object'
+      }
+    });
 
-  Post.remoteMethod('read', {
-    accepts: {
-      arg: 'id',
-      type: 'string',
-      required: true
-    },
-    http: {
-      verb: 'get'
-    },
-    returns: {
-      root: true,
-      type: 'object'
-    }
-  });
-
-  Post.remoteMethod('feed', {
-    http: {
-      verb: 'get'
-    },
-    returns: {
-      root: true,
-      type: 'object'
-    }
-  });
-
-};
+  };
