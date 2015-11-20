455209c0a1e3adc43d51c2dc0c205728383ed10c
Paulo McNally <paulomcnally@gmail.com>
2015-11-20 15:59:07 -0600

    set dev env

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,10 +1,10 @@
 module.exports = {
   db: {
-    host: process.env.INFORMANTE_DB_HOST,
-    port: process.env.INFORMANTE_DB_PORT,
-    database: process.env.INFORMANTE_DB_NAME,
-    username: process.env.INFORMANTE_DB_USERNAME,
-    password: process.env.INFORMANTE_DB_PASSWORD,
+    host: process.env.INFORMANTE_DEV_DB_HOST,
+    port: process.env.INFORMANTE_DEV_DB_PORT,
+    database: process.env.INFORMANTE_DEV_DB_NAME,
+    username: process.env.INFORMANTE_DEV_DB_USERNAME,
+    password: process.env.INFORMANTE_DEV_DB_PASSWORD,
     name: 'db',
     connector: 'mongodb'
   }
