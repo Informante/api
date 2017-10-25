ef9a855a2a3be84cb6f8d49ec33fd78b9707ea0b
Paulo McNally <paulomcnally@gmail.com>
2017-10-25 10:30:34 -0600

    Change database

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,11 +1,6 @@
 module.exports = {
-  db: {
-    host: process.env.INFORMANTE_DEV_DB_HOST,
-    port: process.env.INFORMANTE_DEV_DB_PORT,
-    database: process.env.INFORMANTE_DEV_DB_NAME,
-    username: process.env.INFORMANTE_DEV_DB_USERNAME,
-    password: process.env.INFORMANTE_DEV_DB_PASSWORD,
-    name: 'db',
-    connector: 'mongodb'
+  "db": {
+    "name": "db",
+    "connector": "memory"
   }
 };
--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,6 @@
 module.exports = {
   db: {
-    host: process.env.INFORMANTE_DB_HOST,
-    port: process.env.INFORMANTE_DB_PORT,
-    database: process.env.INFORMANTE_DB_NAME,
-    username: process.env.INFORMANTE_DB_USERNAME,
-    password: process.env.INFORMANTE_DB_PASSWORD,
-    name: 'db',
-    connector: 'mongodb'
+    url: process.env.DATABASE_URL,
+    connector: 'postgresql',
   }
 };
